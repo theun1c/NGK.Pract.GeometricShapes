9cfcbd1eeea9d07be4662b49c4421ce066279907
theun1c <belovdima1122@yandex.ru>
2025-05-14 01:32:38 +0300

    init repo

---
+++ NGK.Pract.GeometricShapes/Model/Triangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NGK.Pract.GeometricShapes.Model
+{
+    /// <summary>
+    /// Модель для треугольника
+    /// </summary>
+    public class Triangle
+    {
+        public double SideOne { get; set; }
+        public double SideTwo { get; set; }
+        public double SideThree { get; set; }
+
+    }
+}
