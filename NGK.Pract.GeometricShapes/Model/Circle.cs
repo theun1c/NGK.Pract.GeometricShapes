2161e2f98ba0479ce4c2a98d7f3903108f8399bb
theun1c <belovdima1122@yandex.ru>
2025-05-14 01:40:26 +0300

    create some citcle methods

--- NGK.Pract.GeometricShapes/Model/Circle.cs
+++ NGK.Pract.GeometricShapes/Model/Circle.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace NGK.Pract.GeometricShapes.Model
 {
-    internal class Circle
+    /// <summary>
+    /// Модель для круга
+    /// TODO: методы для вычисления площади и периметра
+    /// </summary>
+    public class Circle
     {
+        /// <summary>
+        /// конструктор, принимающий значение радиуса
+        /// </summary>
+        /// <param name="radius"></param>
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+        public double Radius { get; set; }
+        public double Diameter => Radius * 2;  
+
+        /// <summary>
+        /// метод для подсчета периметра круга
+        /// </summary>
+        /// <returns></returns>
+        public double Perimetr()
+        {
+            return Diameter * Math.PI;
+        }
+
+        /// <summary>
+        /// метод для подсчета площади круга
+        /// </summary>
+        /// <returns></returns>
+        public double Ploshad()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
     }
 }
